66bcb5fdee89bbbf4f5b20009f6ed3de476677e0
Christopher Aguiar <epicchris17@gmail.com>
2019-12-04 12:55:57 -0500

    Not done still on 1st

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,21 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let p = Number(document.getElementsByClassName("pu"));
+  let q = Number(document.getElementsByClassName("qty"));
+
+  let t = document.getElementById("inp").value;
+
+  subtotal = p * q;
+  document.getElementsByClassName("subtot").innerHTML = subtotal;
+
+  return subtotal;
+
 }
 
 function calcAll() {
   // Iteration 1.2
+  
+
 }
 $calc.onclick = calcAll;
