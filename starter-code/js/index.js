8ec3ba760e397de0881f6fb66b4c78ef3ab70fbd
Christopher Aguiar <epicchris17@gmail.com>
2019-12-11 12:44:09 -0500

    Done completely

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,29 +1,29 @@
-let $cart = document.querySelector('#cart tbody');
-let $calc = document.getElementById('calc');
-
+let $cart = document.querySelector("#cart tbody");
+let $calc = document.getElementById("calc");
 
 console.log($cart);
 
+console.log($calc);
 
 function updateSubtot($product) {
   // Iteration 1.1
   let unitPrice = $product.querySelector(".pu span").innerHTML;
   unitPrice = Number(unitPrice);
-  
+
   let quantity = $product.querySelector(".qty input").value;
   quantity = Number(quantity);
-  
+
   let subtotal = unitPrice * quantity;
-  
+
   let subtotEle = $product.querySelector(".subtot span");
   subtotEle.innerHTML = subtotal;
-  
+
   return subtotal;
 }
 
 function calcAll() {
   // Iteration 1.2
-  
+
   let productsCollection = $cart.getElementsByClassName("product");
   let $product;
   let sumSubtotal = 0;
@@ -37,23 +37,67 @@ function calcAll() {
   total.innerHTML = sumSubtotal;
 }
 
+$calc.onclick = calcAll;
 
+deleteThing();
 
-let deleteProductList = $cart.getElementsByClassName("btn btn-delte");
+function deleteThing(){
+
+let deleteProductList = $cart.getElementsByClassName("btn btn-delete");
 
 for (let i = 0; i < deleteProductList.length; i++) {
   let deleteList = deleteProductList[i];
-
-  deleteList.addEventListener("click", function(event) {
-
+  deleteList.addEventListener("click", function(element) {
     let buttonClicked = event.target;
     buttonClicked.parentElement.parentElement.remove();
-    calcAll()
+    calcAll();
   });
+}
 
 }
 
 
+//  Creation
 
-$calc.onclick = calcAll;
 
+document.getElementById("create").addEventListener("click", function(){
+
+  let inputName = document.getElementsByClassName("new")[0].getElementsByTagName("input")[0].value;
+  let inputPrice = Number(document.getElementsByClassName("new")[0].getElementsByTagName("input")[1].value);
+  
+  
+  document.querySelector('#cart tbody').innerHTML += `<tr class="product">
+<td class="name">
+  <span>${inputName}</span>
+</td>
+<td class="pu">
+  $<span>${inputPrice}</span>
+</td>
+<td class="qty">
+  <label>
+    <input type="number" value="0" min="0">
+  </label>
+</td>
+<td class="subtot">
+  $<span>0</span>
+</td>
+<td class="rm">
+  <button class="btn btn-delete">Delete</button>
+</td>
+</tr>`
+
+deleteThing();
+calcAll()
+});
+
+
+
+
+  
+  
+  
+  
+
+
+
+ 
